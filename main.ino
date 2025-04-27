bd851ac165efb78bc0f79ace9906429c9a7582d5
Loran-ash <aaaloran383@gmail.com>
2025-04-28 01:02:06 +0800

    add http template

--- main.ino
+++ main.ino
@@ -1,9 +1,54 @@
-void setup()
-{
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+void setup(){
+
 	pinMode(10, OUTPUT);
+	
+    Serial.begin(115200)
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    // Print local IP address and start web server
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
-void loop()
-{
-	
+void loop(){
+    //Check WiFi connection status
+
+    HTTPClient http;
+    String serverPath = "http://192.168.1.100:5000/index"; //replace with server's IP address
+
+    http.begin(serverPath); //GET request
+    int httpCode = http.GET(); 
+
+    if (httpCode > 0){ //If alright
+        //TODO
+        Serial.print("Content-Type = ");
+        Serial.println(http.header("Content-Type"));
+    
+        Serial.print("Content-Length = ");
+        Serial.println(http.header("Content-Length"));
+
+    }else{
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    
+    http.end();
+    
+    delay(5000);  //send request every 5s
+
 }
--- setup.md
+++ setup.md
@@ -5,3 +5,17 @@ nice to see u
 [Live Share](https://marketplace.visualstudio.com/items/?itemName=MS-vsliveshare.vsliveshare) 作為出現問題即時共作,  
 另外我採用[git](https://git-scm.com/)和[github](https://github.com/)作為非同步共作,  
 其中這次的repo是:https://github.com/wuni1102/makeNTU2025.git
+
+目前打算esp32當client
+待完成:
+
+server
+1. 傳送移動、舉手指令
+2. 語音辨識
+3. 事先儲存動作組合(弄個UI?)
+...
+
+client
+1. 隔一段時間向server送請求
+2. 處理傳過來的指令
+...
